06b891743176df7dfa7743c09acbbb5cb71da28f
Doggie <76987398+DoggieLicc@users.noreply.github.com>
2023-02-19 13:38:51 -0500

    Api tests

--- application/wsgi.py
+++ application/wsgi.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, request
 
 application = Flask(__name__)
 
-@application.route("/api/")
+@application.route("/api/test")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@application.route("/api/ip")
+def get_ip():
+	return f"<h1>Your IP is: {request.remote_addr}</h1>"
